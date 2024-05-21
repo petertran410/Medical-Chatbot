b2da84321c6f88e3f2f57990ccd570e81392f0e3
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2024-05-22 00:02:25 +0700

    .

--- Streamlit/model.py
+++ Streamlit/model.py
@@ -47,7 +47,8 @@ def load_llm():
 
 
 def qa_bot(query):
-    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2",
+    # sentence-transformers/all-MiniLM-L6-v2
+    embeddings = HuggingFaceEmbeddings(model_name="imdeadinside410/Llama2-Syllabus",
                                        model_kwargs={'device': 'cpu'})
     db = FAISS.load_local(DB_FAISS_PATH, embeddings)
     llm = load_llm()
